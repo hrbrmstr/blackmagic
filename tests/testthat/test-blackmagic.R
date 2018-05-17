a0938cb5191237f5ed3a4a4a18d36f82e752f2ba
boB Rudis <bob@rud.is>
2018-05-17 05:17:47 -0400

    simple tests

--- tests/testthat/test-blackmagic.R
+++ tests/testthat/test-blackmagic.R
@@ -1,6 +1,11 @@
 context("minimal package functionality")
 test_that("we can do something", {
 
-  #expect_that(some_function(), is_a("data.frame"))
+  xml <- '<?xml version="1.0" encoding="utf-8"?><note importance="high"><title>Happy</title></note>'
+
+  expect_equal(
+    abbreviate(xml_to_json(xml), minlength=50, method = "both.sides", named=FALSE),
+    "{\"\":{\"\":{\"\":\"1.0\",\"\":\"-8\"}},\"\":[{\"\":\"\",\"\":\"\",\"\":{\""
+  )
 
 })
